0bee955e564138d0e8bda8338a5305b01f7d4a47
«Andrey» <khomehko@mail.ru>
2022-08-18 02:01:11 +0300

    h25

--- h25/Program.cs
+++ h25/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// Не использовать Math.Pow() и аналоги!
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int countb = b-1;
+long nata =a;
+
+while (countb>=1)
+{
+nata=nata*a;
+countb=countb-1;
+}
+
+
+Console.WriteLine(nata);
