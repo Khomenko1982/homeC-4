e6691043b1cf5cfbb89b89cb2006d239a07ce073
«Andrey» <khomehko@mail.ru>
2022-08-18 04:24:02 +0300

    h29

---
+++ h30/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] arr =new int[8];
+int i=0;
+for ( i=0; i<8; i++)
+{
+    int a = new Random().Next(0,2);
+    arr[i]=a;
+    Console.Write(arr[i]);
+    Console.Write(" ");
+}
+// Console.WriteLine(arr[]);
